7e5fd4e3e6f176c4b0ce95fb41e6472a213986d9
Andres-IAGC <71302151+Andres-IAGC@users.noreply.github.com>
2020-11-25 12:57:06 -0800

    Empezamos con el Hola Mundo
    Primer commit

---
+++ Figuras/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
